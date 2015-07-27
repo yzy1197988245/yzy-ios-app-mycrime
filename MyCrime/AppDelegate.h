2446c4792ad072d9c5212688931a3cd9f9af4985
yzy <1197988245@qq.com>
2015-07-27 14:31:41 +0800

    Initial Commit

---
+++ MyCrime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCrime
+//
+//  Created by mini2 on 15/7/27.
+//  Copyright (c) 2015年 yzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCrime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCrime
+//
+//  Created by mini2 on 15/7/27.
+//  Copyright (c) 2015年 yzy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
